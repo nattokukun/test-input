02d579b3e0747693906951177c049a528168476d
nattokukun <idama.jp@gmail.com>
2022-11-15 16:53:03 +0900

    refactor 02 try04

--- launcher/app.py
+++ launcher/app.py
@@ -1,34 +1,38 @@
 # -*- coding: utf-8 -*-
 
-"""
-xxx_dir = 'launcher'
-
-import sys
-sys.path.append(xxx_dir)
-
-## kvファイルの明示的なロード
+#######
 from kivy.lang import Builder
-## self.root = Builder.load_file(xxx_dir+"/"+"test.kv")
-Builder.load_file(xxx_dir+"/"+"test.kv")
-"""
+#######
 
-
+from kivy import Config
+Config.set('graphics', 'multisamples', '0')
 
 from kivy.app import App
+from kivy.uix.label import Label
 
 class Launcher(App):
+
     def build(self):
 
-        xxx_dir = 'launcher'
 
+        #############
         import sys
-        sys.path.append(xxx_dir)
-        """
-        """
+        sys.path.append('launcher')
+        #############
 
+
+        #############
         ## kvファイルの明示的なロード
-        from kivy.lang import Builder
-        self.root = Builder.load_file(xxx_dir+"/"+"test.kv")
+        self.root = Builder.load_file("launcher/test.kv")
+        ##self.root = Builder.load_file("test.kv")
+        #############
+
+
+        ## print('[1]')
+        ## return Label(text="Hello World")
+
+        #############
+        # from launcher.app import Launcher
 
         from mainxx import MainForm
         tp_mf = MainForm()
